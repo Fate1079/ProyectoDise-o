cb9a51f241df61a1d7e40e06cbad0c11a34ae22c
Fate <andresfeli107911@gmail.com>
2025-02-24 15:25:18 -0500

    commit inicial

---
+++ src/main/java/com/mycompany/proyectodise/ProyectoDise.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectodise;
+
+/**
+ *
+ * @author ANDRES FELIPE
+ */
+public class ProyectoDise {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
